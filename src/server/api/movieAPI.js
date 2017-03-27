f46af6cdace03faefff29e1473e4eb3b1a954d21
Chad Martin <Chad Martin>
2017-03-27 14:48:43 -0700

    added movieAPI for intro to node

--- src/server/api/movieAPI.js
+++ src/server/api/movieAPI.js
@@ -2,3 +2,41 @@
 /** @todo: add Server Side Movie Services goes here
 *   This class will be where the you put Client side services
 */
+export default class MovieAPI
+{
+static getMovieList ()
+{
+  return [
+    {  id: 1017109,
+      title: 'Juno',
+      genre: 'Drama',
+  },
+  {
+      id: 1017105,
+      title: 'Star Wars',
+      genre: 'Sy-Fy',
+  },
+  {
+      id: 1017108,
+      title: 'Big',
+      genre: 'Sy-FY',
+  },
+  {
+      id: 1017104,
+      title: 'Juno',
+      genre: 'Drama',
+  },
+  {
+      id: 1017106,
+      title: 'IRObot',
+      genre: 'Sy-FY',
+
+  },
+  {
+      id: 4444441,
+      title: 'Logan',
+      genre: 'Sy-Fy',
+  }
+    ]
+}
+}
